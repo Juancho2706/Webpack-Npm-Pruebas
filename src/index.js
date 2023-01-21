727246932f33319880e4df7603f1555074b6bcfe
Juan Villegas <jvillegas.dev@gmail.com>
2023-01-21 14:01:27 -0300

    Learning assets management

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import myName from './myName';
+import '../src/style.css';
+import laImagen from '../src/image23.png';
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component(){
     const element = document.createElement('div');
 
     // Using the already imported function i did on myName.js 
     element.innerHTML = myName('Cody');
+    element.classList.add('hello');
+
+    //adding our new image to the module export
+    const myImagen = new Image();
+    myImagen.src = laImagen;
+
+    element.appendChild(myImagen);
+
+    console.log(Data);
+    console.log(Notes);
 
     return element
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,31 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    module:{
+        rules:[
+            { //to handle css
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            { //to handle images
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            { //to handle fonts
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            { //to handle csv or tsv archives
+                test: /\.(csv|tsv)$/i,
+                use: ['csv-loader'],
+            },
+            { //to handle xml
+                test: /\.xml$/i,
+                use: ['xml-loader'],
+            },
+        ]
+    }
 };
